c5cbe1eef396ff1073530cea32beef4553db60c2
David Taylor <djtaylor13@gmail.com>
2015-10-07 09:27:04 -0400

    Updated MANIFEST; Restructured layout for Debian packaging; Created debian directory

--- setup.py
+++ setup.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python
-import lense.client as lense_client
 from setuptools import setup, find_packages
 
-# Module version / long description
-version = lense_client.__version__
-long_desc = open('DESCRIPTION.rst').read()
+# Import the module version
+from lense.client import __version__
 
 # Run the setup
 setup(
-    name='lense-client',
-    version=version,
-    description='Lense API platform client libraries',
-    long_description=long_desc,
-    author='David Taylor',
-    author_email='djtaylor13@gmail.com',
-    url='http://github.com/djtaylor/lense-client',
-    license='GPLv3',
-    packages=find_packages(),
-    keywords='lense api client platform engine',
-    classifiers=[
+    name             = 'lense-client',
+    version          = __version__,
+    description      = 'Client Python libraries and scripts for the Lense API engine',
+    long_description = open('DESCRIPTION.rst').read(),
+    author           = 'David Taylor',
+    author_email     = 'djtaylor13@gmail.com',
+    url              = 'http://github.com/djtaylor/lense-client',
+    license          = 'GPLv3',
+    install_requires = ['lense-common>=0.1.1'],
+    packages         = find_packages(),
+    keywords         = 'lense module library client',
+    classifiers      = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Terminals',
-    ],
-    install_requires = [
-        'lense-common>=0.1'
     ],
     entry_points = {
         'console_scripts': [
