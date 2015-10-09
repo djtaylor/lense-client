55b76532727a021e378f342ddfc5df8ef9e150dc
David Taylor <djtaylor13@gmail.com>
2015-10-09 09:52:04 -0400

    Renamed references to old package name in bootstrap module

--- usr/lib/python2.7/dist-packages/lense/client/module/bootstrap.py
+++ usr/lib/python2.7/dist-packages/lense/client/module/bootstrap.py
@@ -1,8 +1,8 @@
-from cloudscape.common.bootstrap.manager import Bootstrap
+from lense.common.bootstrap.manager import Bootstrap
 
 class ModBootstrap:
     """
-    Client module for bootstrap the Cloudscape installation.
+    Client module for bootstrap the Lense installation.
     """
     def __init__(self, parent):
         self.parent = parent
