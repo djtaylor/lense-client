44ac7767d62724159081b22ad3e23f8a44f30fe9
David Taylor <djtaylor13@gmail.com>
2015-10-27 09:14:14 -0400

    Added connector module

---
+++ usr/lib/python2.7/dist-packages/lense/client/module/connector.py
@@ -0,0 +1,30 @@
+class ModConnector:
+    """
+    Class wrapper for requests to the connector utilities.
+    """
+    def __init__(self, parent):
+        self.parent = parent
+        
+    def get(self, data={}):
+        """
+        Get details for either a single or all connectors.
+        """
+        return self.parent._get('connector', data=data)
+    
+    def create(self, data={}):
+        """
+        Create a new connector.
+        """
+        return self.parent._post('connector', data=data)
+    
+    def updated(self, data={}):
+        """
+        Update an existing connector.
+        """
+        return self.parent._post('connector', data=data)
+    
+    def delete(self, data={}):
+        """
+        Delete an existing connector.
+        """
+        return self.parent._delete('connector', data=data)
