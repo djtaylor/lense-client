1e279c43b82182db5dfbb4621147eb68844b7683
David Taylor <djtaylor13@gmail.com>
2016-01-12 11:21:43 -0500

    Migrating to new client libraries

--- usr/lib/python2.7/dist-packages/lense/client/__init__.py
+++ usr/lib/python2.7/dist-packages/lense/client/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.1.1'
+from lense.client.handler import ClientHandler_CLI
+
+def cli():
+    """
+    Load the client CLI handler.
+    """
+    client = ClientHandler_CLI()
+    client.run()
+    
